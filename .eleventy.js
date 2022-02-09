bf1d329a5861c05572a51f04e565b0eae70a218f
vxncetxn <thevancetan@gmail.com>
2022-02-09 23:13:51 +0800

    Added build-time image optimizations via 11ty Image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,25 @@
+const Image = require('@11ty/eleventy-img');
+
+const imageShortcode = async (src, alt) => {
+    const metadata = await Image(src, {
+        widths: [300, 600, 1200],
+        formats: ['avif', 'webp', null],
+        outputDir: '_site/img',
+    });
+
+    const imageAttributes = {
+        alt,
+        sizes: '(max-width: 600px) 300px, 600px',
+        loading: 'lazy',
+        decoding: 'async',
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+};
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('public');
+    eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
     return {
         markdownTemplateEngine: 'njk',
